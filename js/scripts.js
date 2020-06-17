0db3f594a425e2d7d18c79620e3585acaec97f90
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-16 22:22:13 -0700

    Add constructor function to scripts.js business logic as templace for creating new library objects.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+//BUSINESS LOGIC
+
+//business logic to make a new library -> with the property books set to hold an array
+function NewLibrary() {
+//this.property = value
+  this.books = [];
+  this.currentId = 0;
+}
