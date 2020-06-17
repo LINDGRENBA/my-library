42a8d5b311ab485158631c44942eb660e2487493
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-16 23:27:39 -0700

    Update notes.js with next step in process, create custom method in business logic to push each new book object to books array in brittsLibrary, add line in user interface to call new custom method.

--- js/notes.js
+++ js/notes.js
@@ -12,5 +12,12 @@
 
   4. Create first instance of NewBook within .submit function. Every time new-book form is submitted, it will create a new instance of NewBook / aka a new NewBook object.
 
+  5. In the business logic, create a custom method that will act on any NewLibrary. This custom method will push the NewBook objects to the array in the library. Then call it from the user interface logic
+    a) create the custom method -> EXAMPLE: NewLibrary.prototype.addBookToLibrary(bookToAdd){this.books.push(bookToAdd)};
+    b) in the user interface .submit, use the custom method on your library -> EXAMPLE: brittsLibrary.addBookToLibrary(newBookObject) - this passes a new object to be pushed each time the form is submitted
+  
+  
+
+
 
   */
--- js/scripts.js
+++ js/scripts.js
@@ -14,25 +14,24 @@ function NewBook(title, author) {
 }
 
 //business logic to push each new book object created with NewBook constructor to books array in library
-
-
-
-
-
-
-
+NewLibrary.prototype.addBookToLibrary = function(bookToAdd){
+  this.books.push(bookToAdd);
+}
+//need to call above custom method on brittsLibrary and pass argument of the book title? or book object?
 
 
 //USER INTERFACE LOGIC
 
 $(document).ready(function() {
-  let brittsLibrary = new NewLibrary();
+  let brittsLibrary = new NewLibrary(); // first instance of NewLibrary constructor
   $("#add-book").submit(function(event) {
     event.preventDefault();
 
     let bookTitleInput = $("#book-title").val();
     let bookAuthorInput = $("#book-author").val();
-    let newBookObject = new NewBook(bookTitleInput, bookAuthorInput);
+    let newBookObject = new NewBook(bookTitleInput, bookAuthorInput); //first instance of NewBook constructor
+    brittsLibrary.addBookToLibrary(newBookObject); // target brittsLibrary, use addBookToLibrary function by calling it and passing the newBookObject to it
+    //now brittsLibrary should hold each submission as an object within its books array
 
   });
 });
